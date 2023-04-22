b0e7bffce9c031f20a0b852c877efdb8e28f4d81
ksowah <sowahkelvin640@gmail.com>
2023-04-22 12:06:39 +0000

    Final build

---
+++ components/About.tsx
@@ -0,0 +1,69 @@
+const About_Block = () => {
+  return (
+    <div className="py-[2rem] w-full bg-white md:px-[8rem]">
+      <div className="p-[4px] md:px-[2rem]">
+        <h1 className="text-blacl text-center text-[2rem] font-bold">
+          Background Statement
+        </h1>
+
+        <h1 className="text-blacl text-center text-[1.2rem] font-bold mt-4">
+          Digital Payments and Mobile Wallets
+        </h1>
+
+        <h2 className="text-blacl text-[1.5rem] text-primary font-bold mt-[2rem] ">
+          Electronic Cheque System (eCheque)
+        </h2>
+        <div>
+          In a world where every nation is moving cashless, team Pukka has come
+          up with an Artificial Intelligence electronic Cheque system. With our
+          eCheque system, you can snap a cheque and verify its validity before
+          heading to bank. Not just that but you can as well be at the comfort of your home and transfer
+          money into your bank account without any charges.
+        </div>
+        <h2 className="text-blacl text-[1.5rem] text-primary font-bold mt-[2rem] ">
+          Why Cheque is still in use?
+        </h2>
+        <p>
+          Ghana&rsquo;s market for mobile money services has seen the most growth in the country&rsquo;s digital financial services sector.
+          As the use of mobile money increases, fraudulent activity also keeps occurring. When compared to
+          carrying cash, checks are thought to be safer and more practical. Additionally, if your check is lost,
+          nobody can use it. According to at least one research, the typical bank line-up time exceeds 20 minutes.
+          It occasionally lags for up to 40 minutes. As a result, our AI software will shorten lines at the bank and enable
+          more remote customers to conduct typical business without visiting the institution. This will significantly increase financial inclusion in Ghana and assist in reducing
+          the additional fees the e-levy levies on users of mobile money.
+        </p>
+      </div>
+
+      <div className="p-[4px] md:px-[2rem]">
+        <h2 className="text-blacl text-[1.5rem] text-primary font-bold mt-[2rem] ">
+          How eCheque Works
+        </h2>
+        <h4 className="text-[1.3rem] font-medium">
+          Three conditions are met before a cheque is verified:
+        </h4>
+        <p className="md:ml-[2rem]">
+          1. The payee should have an account with the bank.
+        </p>
+        <p className="md:ml-[2rem]">
+          2. An image processing model filters the cheque, extract the signature
+          and compares it with the signature in the banks database to checks if
+          the structural similarity is greater or equal to 90%.
+        </p>
+        <p className="md:ml-[2rem]">
+          3. Last condition, the amount in the payee’s account is greater or
+          equals to the amount written on the cheque.
+        </p>
+      </div>
+
+      <div className="p-[4px] md:px-[2rem]">
+        <h2 className="text-blacl text-[1.5rem] text-primary font-bold mt-[2rem]">
+          Security Features on the Cheque (QR)
+        </h2>
+        <p className="md:ml-[2rem]">1. Upload Identity Card.</p>
+        <p className="md:ml-[2rem]">2. Confirmation from drawer.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About_Block;
---
+++ components/showcase/index.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import Router, { useRouter } from "next/router";
+import { motion } from "framer-motion";
+
+const Showcase = ({ remove }: { remove?: boolean }) => {
+  const router = useRouter();
+
+  // move to upload page
+  const handleUpload = () => {
+    Router.push("/upload");
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 200 }}
+      className="flex w-full mt-5 pb-6 gap-y-3 md:gap-y-0 flex-col md:flex-row items-center"
+    >
+      <motion.div
+        initial={{ opacity: 0, x: 200 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 200 }}
+        className="w-full md:w-[60%]  flex items-center justify-center"
+      >
+        <div className="w-[90%] md:w-[60%]">
+          <h1 className="text-3xl md:text-4xl font-bold text-white">
+            The easiest way to manage your personal finances
+          </h1>
+          <p className="text-gray-100 mt-4 text-sm md:text-lg">
+            With our AI eCheque system, you can snap a cheque and verify
+            its validity before heading to bank; you can be at the comfort of
+            your home and transfer money into your bank account without any
+            charges.
+          </p>
+          {!remove && (
+            <div className="flex flex-col md:flex-row mt-8 gap-y-3 md:gap-x-3">
+              <motion.button
+                whileHover={{ scale: 1.02 }}
+                onClick={handleUpload}
+                className="flex items-center justify-center gap-x-2 bg-active text-primary px-4 py-2 rounded-sm text-base font-semibold"
+              >
+                Upload Cheque
+                <div className="p-2 rounded-full bg-white">
+                  <BsArrowRight />
+                </div>
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.02 }}
+                className="bg-white text-primary px-4 py-2 rounded-sm text-base font-semibold"
+                onClick={() => router.push("/about")}
+              >
+                About Pukka
+              </motion.button>
+            </div>
+          )}
+        </div>
+      </motion.div>
+      <div className="w-[90%] md:w-[40%] flex items-center justify-center">
+        <div className="w-80">
+          <motion.img
+            src="/assets/banner.png"
+            alt="showcase"
+            className="w-full h-full object-contain cursor-pointer"
+            whileHover={{ scale: 1.02 }}
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Showcase;
